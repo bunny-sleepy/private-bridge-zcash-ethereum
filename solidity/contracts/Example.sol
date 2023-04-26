fa0b0fba501c9a9482ec536caad6d46a5d8b5244
Wenhao Wang <wang142857@hotmail.com>
2023-04-26 17:32:37 +0000

    solidity main contract updates

--- solidity/contracts/Example.sol
+++ solidity/contracts/Example.sol
@@ -4,14 +4,20 @@ pragma solidity >= 0.8.0;
 import {IVerifier} from "./Interface/IVerifier.sol"; 
 import {IzkBridge} from "./Interface/IzkBridge.sol";
 import {IMockToken} from "./Interface/IMockToken.sol";
+import {Base58} from "./Dependency/storyicon/Base58.sol";
 
 contract Example {
     IMockToken _token;
     IVerifier _verifier;
     IzkBridge _zkBridge;
+    // TODO: change these values
+    uint constant bytesBefore = 1;
+    uint constant bytesBetween = 1;
+    uint constant bytesAfter = 1;
+    uint constant depth = 2;
     mapping(address => bool) internal _writePermission;
     mapping(address => mapping(uint256 => bytes)) internal _lockAddress;
-    mapping(address => mapping(uint256 => uint256)) internal _lockValue;
+    mapping(address => mapping(uint256 => uint64)) internal _lockValue;
     mapping(address => mapping(uint256 => bool)) internal _isValidLockAddress;
     mapping(address => uint256) internal _maxIndexLockAddress;
 
@@ -47,32 +53,133 @@ contract Example {
         return _lockAddress[user][index];
     }
 
+    function bytesToUint64(bytes memory b) public pure returns (uint64) {
+        require(b.length == 8, "Bytes array length must be 8");
+
+        uint64 result;
+        assembly {
+            result := mload(add(b, 8))
+        }
+        return result;
+    }
+
+
+    function bitArrayToBytes(uint256[8][] memory bits) internal pure returns (bytes memory) {
+        uint256 n = bits.length;
+        bytes memory bytesArr = new bytes(n);
+        for (uint256 index = 0; index < n; index++) {
+            for (uint8 j = 0; j < 8; j++) {
+                if (bits[index][j] == 1) {
+                    bytesArr[index] |= bytes1(uint8(1) << j);
+                }
+            }
+        }
+        return bytesArr;
+    }
+
+    function bitcoinPubkeyhashAddressConsistent(
+        bytes memory pubKeyHash,
+        bytes memory _addr
+    ) internal pure returns (bool) {
+        bytes memory addr = Base58.decode(_addr);
+        if (addr.length != 25) {
+            return false;
+        }
+        if (pubKeyHash.length != 20) {
+            return false;
+        }
+        // we allow only P2PKH address
+        if (addr[0] != 0x00) {
+            return false;
+        }
+        bytes memory bytesArr = new bytes(20);
+        for (uint i = 0; i < 20; i++) {
+            bytesArr[i] = addr[i+1];
+        }
+        if (keccak256(pubKeyHash) != keccak256(bytesArr)) {
+            return false;
+        }
+        return true;
+    }
+
+    struct VerifierInput {
+        uint256[4][8] CONSENSUS_BRANCH_ID;
+        uint256[32][8] header_digest;
+        uint256[32][8] prevouts_digest;
+        uint256[32][8] sequence_digest;
+        uint256[32][8] sapling_digest;
+        uint256[32][8] orchard_digest;
+        uint256[bytesBefore][8] bytes_before;
+        uint256[8][8] value;
+        uint256[bytesBetween][8] bytes_between;
+        uint256[20][8] pubKeyHash;
+        uint256[bytesAfter][8] bytes_after;
+        // mtp signals
+        uint256[256] root;
+        uint256[depth - 1][256] merklePath;
+        // neighbor of leaf in the last layer
+        uint256[256] neighbor;
+        // index[i] = 0 means hash / leaf is on the left; 1 otherwise
+        uint256[depth] index;
+    }
+
+    struct VerifyAndMintInput {
+        address onBehalfOf;
+        uint256 index;
+        uint256 blockNumber;
+        uint[2] a;
+        uint[2][2] b;
+        uint[2] c;
+        // uint[68] memory input;
+        VerifierInput verifierInput;
+    }
+
     function VerifyAndMint(
-        address onBehalfOf,
-        uint256 index,
-        uint[2] memory a,
-        uint[2][2] memory b,
-        uint[2] memory c,
-        uint[68] memory input,
-        uint256 blockNumber
+        VerifyAndMintInput memory input
     ) external returns (bool) {
-        // 0. TODO: get value, pubKeyHash of lockAddress from txData
+        // 0. get value, pubKeyHash of lockAddress from txData
         address user = msg.sender;
-        require(_isValidLockAddress[user][index] == true);
-        uint256 value = 0;
-        bytes memory pubKeyHash;
-        bytes memory lockAddress = _lockAddress[user][index];
-        // 1. TODO: verify pubKeyHash and lockAddress are consistent; value > 0
-        require(value > 0);
-        require(keccak256(pubKeyHash) == keccak256(lockAddress));
-        _lockValue[user][index] = value;
+        require(_isValidLockAddress[user][input.index] == true);
+        require(_lockValue[user][input.index] == uint64(0));
+        uint64 value;
+        bytes memory pubKeyHashBytes;
+        {
+            uint256[8][] memory tmp_value = new uint256[8][](8);
+            for (uint i = 0; i < 8; i++) {
+                for (uint j = 0; j < 8; j++) {
+                    tmp_value[i][j] = input.verifierInput.value[i][j];
+                }
+            }
+            bytes memory valueBytes = bitArrayToBytes(tmp_value);
+            value = bytesToUint64(valueBytes);
+        }
+        // convert pubKeyHash to tmp dynamic 2-d array
+        {
+            uint256[8][] memory tmp_pubKeyHash = new uint256[8][](20);
+            for (uint i = 0; i < 20; i++) {
+                for (uint j = 0; j < 8; j++) {
+                    tmp_pubKeyHash[i][j] = input.verifierInput.pubKeyHash[i][j];
+                }
+            }
+            pubKeyHashBytes = bitArrayToBytes(tmp_pubKeyHash);
+            
+        }
+        // 1. verify pubKeyHash and lockAddress are consistent; value > 0
+        {
+            // bytes memory lockAddress = _lockAddress[user][input.index];
+            require(value > 0);
+            bool checkValue = bitcoinPubkeyhashAddressConsistent(pubKeyHashBytes, _lockAddress[user][input.index]);
+            require(checkValue == true);
+        }
+        _lockValue[user][input.index] = value;
 
         // 2. TODO: convert txData to SNARK verifier input
-        bool verifyResult = _verifier.verifyProof(a, b, c, input);
+        uint[68] memory tmp;
+        bool verifyResult = _verifier.verifyProof(input.a, input.b, input.c, tmp);
         require(verifyResult == true);
 
         // 3. mint tokens if pass
-        _token.mint(onBehalfOf, value);
+        _token.mint(input.onBehalfOf, value);
     }
     // (value, address, root)
     function Burn(address onBehalfOf, uint256 index) external {
